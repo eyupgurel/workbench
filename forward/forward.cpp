586acef564733a261c14abbd0c6edda730c2c951
Eyup Gurel <eyupgurel@gmail.com>
2019-05-08 21:38:02 +0300

    outside template forward call samples incorporated

--- forward/forward.cpp
+++ forward/forward.cpp
@@ -17,11 +17,16 @@ void forward::drive() {
 
     A ar{string{"I'am a right value"}};
     string vs{"I can be varied"};
+
     const string cs{"I am constant!"};
     A ars{move(vs)};
     A acs{move(cs)}; //here it calls lvalue overload because if move is applied to "const string" type
                      //it will become const string&& and the A(const string& s) constructor will
                      //win over A(string&& s) because  the compiler respects constantness and it should!
+
+    string& rvs{vs};
+    A fs{std::forward<string>(vs)}; //will call A(const string&& s) constructor because forward deems vs as a rvalue (actually inside the template, template paremeter T reduces to string)
+    A frs{std::forward<string&>(rvs)}; //will call A(const string& s) constructor because forward deems rvs as a lvalue (actually inside the template, template paremeter T reduces to string&)
 
 
 }
